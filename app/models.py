3160e6bd4bc33826700d98c4164706da861059e3
Bigyan <bigyan.kc1@gmail.com>
2019-01-19 14:00:21 -0500

    Database connection successful

--- app/models.py
+++ app/models.py
@@ -7,25 +7,25 @@ class User(db.Model):
 	first_name = db.Column(db.String(60))
 	last_name = db.Column(db.String(60))
 	email = db.Column(db.String(60), index = True, unique = True)
-	password_hash = db.Column(db.String(128))
+	password = db.Column(db.String(128))
 
-	def __init__(self, user_id, first_name, last_name, email, password):
-		self.user_id = user_id
-		self.first_name = first_name
-		self.last_name = last_name
-		self.email = email
-		self.password_hash = generate_password_hash(password)
+	# def __init__(self, user_id, first_name, last_name, email, password):
+	# 	#self.user_id = user_id
+	# 	self.first_name = first_name
+	# 	self.last_name = last_name
+	# 	self.email = email
+	# 	self.password_hash = generate_password_hash(password)
 
-	@property
-	def password(self):
-		raise AttributeError("password is not a readable")
+	# @property
+	# def password(self):
+	# 	raise AttributeError("password is not a readable")
 
-	@password.setter
-	def password(self, password):
-		self.password_hash = generate_password_hash(password)
+	# @password.setter
+	# def password(self, password):
+	# 	self.password_hash = generate_password_hash(password)
 
-	def verify_password(self, password):
-		return check_password_hash(self.password_hash, password)
+	# def verify_password(self, password):
+	# 	return check_password_hash(self.password_hash, password)
 
-	def __repr__(self):
-		return '<User: {}>'.format(self.first_name + self.last_name)
+	# def __repr__(self):
+	# 	return '<User: {}>'.format(self.first_name + self.last_name)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.models import User
+from app import db
+
 @app.route("/")
 def index():
 	return render_template("index.html")
@@ -11,8 +13,9 @@ def new_form():
 
 @app.route("/add_member", methods = ['GET', 'POST'])
 def add_member():
-	user = User(first_name = request.get.form('first_name'), last_name = request.get.form('last_name'), email = request.get.form('email'), password = request.get.form('password'))
+	user = User(first_name = request.form['first_name'], last_name = request.form['last_name'], email = request.form['email'], password = request.form['password'])
 	#add user to the database
 	db.session.add(user)
 	db.session.commit()
+	return("Successfully written to database")
 	
